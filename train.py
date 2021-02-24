a63373819a76d00e249b4396caf9178f007e0eed
distroinfinity <manurajput2911@gmail.com>
2021-02-24 23:41:33 +0530

    Added trainer and model

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
+from model import NeuralNet
+
 with open('intents.json', 'r') as f:
     intents = json.load(f)
 
@@ -26,7 +28,7 @@ allWords = sorted(set(allWords))
 tags = sorted(set(tags))
 # print(allWords)
 # print(tags)
-print(xy)
+# print(xy)
 XTrain = []
 YTrain = []
 for (patternSentence, tag) in xy:
@@ -56,7 +58,48 @@ class CharDataset(Dataset):
 
 # Hyperparameters
 batchSize = 8
+hidden_size = 8
+output_size = len(tags)
+input_size = len(XTrain[0])
+learning_rate = 0.001
+num_epochs = 1000
+
+"""
+print(input_size,len(allWords))
+print(output_size,tags)
+"""
 
 dataset = CharDataset()
 trainLoader = DataLoader(
     dataset=dataset, batch_size=batchSize, shuffle=True, num_workers=2)
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# loss and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# Train the model
+for epoch in range(num_epochs):
+    for (words, labels) in trainLoader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        #labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(words)
+        # if y would be one-hot, we must apply
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    if (epoch+1) % 100 == 0:
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+
+print(f'final loss: {loss.item():.4f}')
